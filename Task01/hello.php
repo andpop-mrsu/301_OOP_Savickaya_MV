c21bc132f3b6216ef9dd468860f296c153033f8a
Savitskaya Margarita <savitskayari@yandex.ru>
2025-02-01 09:43:10 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,6 @@
+<?php
+date_default_timezone_get('Europe/Moscow');
+$dateTime = date("d.m.Y H:i:s");
+$FIO = 'Савицкая Маргарита';
+echo "Текущая дата и время: $dateTime";
+echo "\nФамилия и имя: $FIO";
